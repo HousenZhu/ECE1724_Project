0b04e0a2f213c542f7af9a21e777702e87babc5f
Housen Zhu <benjamin192720@gmail.com>
2025-12-15 18:59:56 -0500

    upload backup api key

--- Windows/src/api_key.rs
+++ Windows/src/api_key.rs
@@ -1,1 +1,3 @@
 pub const DASHSCOPE_API_KEY: &str = "sk-619560edb9664aa5884397e62ac8b712";
+// backup api key if needed
+// pub const DASHSCOPE_API_KEY: &str = "sk-7ae122c42ceb4240aaa7ad34df284362";
